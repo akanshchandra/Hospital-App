30f79005146fe14abed3d2a8092fd8d62d83b388
AKANSH CHANDRA <akanshchandra999@gmail.com>
2024-03-01 11:57:24 +0530

    da

---
+++ src/main/java/org/jsp/hospitalmangementApp/HospitalmanagementAppApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.hospitalmangementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalmanagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalmanagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jsp/hospitalmangementApp/HospitalmanagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.hospitalmangementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalmanagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
